029e77a4f49e3d8b185f1acf15fe49d80f2b8a06
felipealmeidasilva <filipealmeidakukri@hotmail.com>
2025-02-01 14:53:32 -0300

    Ajustanto Admin

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Estoque, EstoqueItens
 
-# Register your models here.
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+	list_display = (
+		'__str__',
+		'funcionario',
+		'nf'
+		)
+	search_fields = ('nf',)
+	list_filter = ('funcionario',)
+	date_hierarchy = 'created'
